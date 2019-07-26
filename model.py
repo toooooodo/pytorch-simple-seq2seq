688eb95b8af386e8fd9d89b444616eca0221d388
toooooodo <1042178105@qq.com>
2019-07-26 19:31:03 +0800

    完善 decoder

--- model.py
+++ model.py
@@ -35,14 +35,12 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, vocab_size, embed_size, enc_hidden_size, dec_hidden_size, batch_size, num_layers=1,
-                 attention_size=10, drop_prob=0):
+    def __init__(self, vocab_size, embed_size, hidden_size, batch_size, num_layers=1, attention_size=10, drop_prob=0):
         super(Decoder, self).__init__()
 
         self.vocab_size = vocab_size
         self.embed_size = embed_size
-        self.enc_hidden_size = enc_hidden_size
-        self.dec_hidden_size = dec_hidden_size
+        self.hidden_size = hidden_size
         self.batch_size = batch_size
         self.num_layers = num_layers
         self.attention_size = attention_size
@@ -50,34 +48,47 @@ class Decoder(nn.Module):
 
         self.embedding = nn.Embedding(self.vocab_size, self.embed_size)
         self.attention = nn.Sequential(
-            nn.Linear(in_features=self.enc_hidden_size + self.dec_hidden_size, out_features=self.attention_size,
+            nn.Linear(in_features=self.hidden_size + self.hidden_size, out_features=self.attention_size,
                       bias=False),
             nn.Tanh(),
             nn.Linear(in_features=self.attention_size, out_features=1, bias=False)
         )
-        self.gru = nn.GRU(input_size=self.embed_size, hidden_size=self.dec_hidden_size, num_layers=self.num_layers,
-                          dropout=self.drop_prob)
+        self.gru = nn.GRU(input_size=self.embed_size + self.hidden_size, hidden_size=self.hidden_size,
+                          num_layers=self.num_layers, dropout=self.drop_prob)
 
-        # self.out = nn.Linear(in_features=0, out_features=self.vocab_size)
+        self.out = nn.Linear(in_features=self.hidden_size, out_features=self.vocab_size)
 
     def forward(self, dec_input, dec_hidden, enc_outputs):
         """
         :param dec_input: [seq_len, batch, input_size]
-        :param dec_hidden: [num_layers * num_directions, batch, hidden_size] => [num_layers, batch, dec_hidden_size]
+        :param dec_hidden: [num_layers * num_directions, batch, hidden_size] => [num_layers, batch, hidden_size]
         :param enc_outputs: encoder outputs,  [seq_len, batch, num_directions * hidden_size]
         :return:
         """
+        # c: [batch_size, hidden_size]
         c = self.attention_forward(dec_hidden, enc_outputs)
+        # input_embedding: [batch_size, seq_len(1), embed_size]
+        input_embedding = torch.transpose(self.embedding(dec_input), 0, 1)
+        # input_embedding: [batch_size, embed_size]
+        input_embedding = torch.squeeze(input_embedding)
+        # input_and_c: [batch_size, embed_size + hidden_size]
+        input_and_c = torch.cat((input_embedding, c), dim=1)
+        # output: [1, batch_size, hidden_size]
+        # hidden: [1, batch_size, hidden_size]
+        output, hidden = self.gru(torch.unsqueeze(input_and_c, dim=0), dec_hidden)
+        # output: [batch_size, vocab_size]
+        output = torch.squeeze(self.out(output), dim=0)
+        return output, hidden
 
     def attention_forward(self, dec_hidden, enc_outputs):
         dec_hiddens, enc_outputs = torch.broadcast_tensors(dec_hidden, enc_outputs)
-        # enc_dec_states shape: [seq_len, batch_size, enc_hidden_size + dec_hidden_size]
+        # enc_dec_states shape: [seq_len, batch_size, hidden_size * 2]
         enc_dec_states = torch.cat((dec_hiddens, enc_outputs), dim=2)
         # e shape: [seq_len, batch_size, 1]
         e = self.attention(enc_dec_states)
         # 对整个输入句子做attention
         alpha = F.softmax(e, dim=0)
-        # c shape: [batch_size, dec_hidden_size]
+        # c shape: [batch_size, hidden_size]
         c = torch.sum(alpha * enc_outputs, dim=0)
         return c
 
@@ -86,13 +97,14 @@ class Decoder(nn.Module):
 
     def init_input(self):
         # 解码器在最初时间步的输入是特殊字符BOS
-        return torch.ones(self.batch_size, 1)
+        i_input = torch.ones(self.batch_size, 1, dtype=torch.int64)
+        return i_input
 
 
 if __name__ == '__main__':
     data_set = F2EDataSet()
-    encoder = Encoder(data_set.in_lang.token_n, embed_size=50, hidden_size=20, batch_size=32)
-    decoder = Decoder(vocab_size=data_set.out_lang.token_n, embed_size=30, enc_hidden_size=20, dec_hidden_size=20,
+    encoder = Encoder(data_set.in_lang.token_n, embed_size=50, hidden_size=64, batch_size=32)
+    decoder = Decoder(vocab_size=data_set.out_lang.token_n, embed_size=30, hidden_size=64,
                       batch_size=32)
     loader = DataLoader(data_set, batch_size=32, shuffle=True)
     for batch_idx, (in_seq, out_seq) in enumerate(loader):
@@ -100,7 +112,6 @@ if __name__ == '__main__':
         enc_outputs, enc_hidden = encoder(in_seq, encoder.init_hidden())
         # enc_outputs of shape (seq_len, batch, num_directions * hidden_size)
         # enc_hidden of shape (num_layers * num_directions, batch, hidden_size)
-        print(enc_outputs.shape, enc_hidden.shape)
-
+        print(f"enc_outputs shape: {enc_outputs.shape}, enc_hidden shape: {enc_hidden.shape}")
         decoder(decoder.init_input(), decoder.init_hidden(enc_hidden), enc_outputs)
         break
